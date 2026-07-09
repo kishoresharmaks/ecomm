c27ab0d868158a657f3921950e89b103b064c6d2
kishoresharmaks <kishoresharma.22it@sonatech.ac.in>
2026-07-09 19:20:57 +0530

    fix: remove BOM from migration file

--- prisma/migrations/20260709200000_add_trial_days/migration.sql
+++ prisma/migrations/20260709200000_add_trial_days/migration.sql
@@ -1,1 +1,1 @@
-﻿ALTER TABLE "seller_subscription_plans" ADD COLUMN "trial_days" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "seller_subscription_plans" ADD COLUMN "trial_days" INTEGER NOT NULL DEFAULT 0;
